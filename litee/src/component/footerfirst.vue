66d6227573cf6677fb80e12fb631cdd9a2fa86b9
brice825KA <brice825KA@users.noreply.github.com>
2026-09-14 15:56:10 +0100

    main finish component footer

--- litee/src/component/footerfirst.vue
+++ litee/src/component/footerfirst.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-    <div class="items-center justify-center flex bg-[#F2F5F7] py-10">
+    <div class="bg-[#F2F5F7] py-5">
         <div class="text-center">
         <div class="items-center justify-center flex pb-5"><img src="../image/Ellipse 3p1.png" alt="ellipse"></div>
         <p>Fini les tracasseries a la banque. Si vous<br>
@@ -9,7 +9,19 @@
             Vraiment cool cette solution.
         </p>
         </div>
+        <div class=" flex justify-center items-center py-1 gap-2 mt-5">
+            <button class="rounded-full bg-[#FA4A4D] w-[10px] h-[10px] t-[336px] l-[713px]"></button>
+            <button class="rounded-full bg-[#C4C4C4] w-[10px] h-[10px] t-[336px] l-[713px]"></button>
+            <button class="rounded-full bg-[#C4C4C4] w-[10px] h-[10px] t-[336px] l-[713px]"></button>
+        </div>
     </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+p {
+    font-family: 'Work Sans' sans-serif;
+    font-weight: 300;
+    font-size: 24px;
+    color: #3E3D53;
+}
+</style>
