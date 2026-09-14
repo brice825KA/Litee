ad0db8bd734690d0f175decf831049490ada8bd3
brice825KA <brice825KA@users.noreply.github.com>
2026-09-14 15:38:18 +0100

    main begin build component footerfirst

--- litee/src/component/footerfirst.vue
+++ litee/src/component/footerfirst.vue
@@ -1,6 +1,15 @@
 <script setup></script>
 
 <template>
+    <div class="items-center justify-center flex bg-[#F2F5F7] py-10">
+        <div class="text-center">
+        <div class="items-center justify-center flex pb-5"><img src="../image/Ellipse 3p1.png" alt="ellipse"></div>
+        <p>Fini les tracasseries a la banque. Si vous<br>
+            essayez Litee, vous en tomberez amoureux.<br>
+            Vraiment cool cette solution.
+        </p>
+        </div>
+    </div>
 </template>
 
 <style scoped></style>
