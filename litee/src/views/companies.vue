c47d80beebd7c8ea71a63df3db50649f6e134069
brice825KA <brice825KA@users.noreply.github.com>
2026-09-14 18:00:18 +0100

    main middle companies page build

--- litee/src/views/companies.vue
+++ litee/src/views/companies.vue
@@ -3,6 +3,43 @@
 </script>
 
 <template>
+    <div class="flex justify-center items-center bg-[#F7FAFB00] pl-30 pr-15">
+        <div class="w-[596px] h-[572px] t-[119px] l-[821px] pt-45">
+            <div class="">
+                <div class="w-[675px] h-[143px] t-[211px] l-[190px]">
+                    <p class="font-family-[Work Sans] font-weight-700 text-[25px] line-height-100% text-[#3E3D53]">
+                        Cartes de paiemnts intelligentes<br>
+                        pour vos transactions partout<br>
+                        dans le monde
+                    </p>
+                </div>
+                <div class="w-[610px] h-[85px] t-[382px] l-[190px]">
+                    <p class="font-family-[Work Sans] font-weight-400 text-[18px] line-height-100% text-[#3E3D53]">
+                        Plus de flexibilite et transparance pour vous<br>
+                        permettre de reprndre le controle de vos depenses
+                    </p>
+                </div>
+            </div>
+            <div class="flex gap-5">
+                <div><input class="pl-5 w-[275px] h-[55px] t-[542px] l-[190px] rounded-[27.5px] border-[1px] border-[#D1D5DB] placeholder:text-[#9CA3AF] placeholder:font-family-[Work Sans] placeholder:font-weight-400 placeholder:text-[16px] placeholder:line-height-100%" type="text" placeholder="Email"></div>
+                <div><button class="w-[172px] h-[55px] t-[542px] l-[475px] rounded-[27.5px] bg-[#FA4A4D] font-family-[Work Sans] font-weight-600 text-[18px] line-height-100% text-[#FFFFFF]">Demande bêta</button></div>
+            </div>
+        </div>
+        <div class="w-[596px] h-[572px] t-[119px] l-[821px]">
+            <img class="w-[596px] h-[572px] t-[119px] l-[821px]" src="../image/dash_litee.png" alt="dash">
+        </div>
+    </div>
+    <!-- Bande -->
+    <div class="bg-[#F2F5F7] w-[1500px] h-[127px] t-[712px] items-center justify-center flex pr-30">
+        <img class="w-[1133px] h-[72px] t-[737px] l-[165px]" src="../image/Group 20.png" alt="bande">
+    </div>
+    <!-- Simple Sentences -->
+    <div class="flex justify-center items-center bg-[#F7FAFB00] pl-30 pr-15 border-b-[1px] border-[#F2F5F7] py-10">
+        <p class="font-family-[Work Sans] font-weight-700 text-[36px] line-height-100% text-[#3E3D53] text-center">
+            En plus des fonctionnalités du<br>
+            compte particulier...
+        </p>
+    </div>
 </template>
 
 <style scoped></style>
