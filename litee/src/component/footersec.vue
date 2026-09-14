e6b30bc6dbb94f9293c829d29e247198ba821508
brice825KA <brice825KA@users.noreply.github.com>
2026-09-14 13:40:29 +0100

    main begin footer second

--- litee/src/component/footersec.vue
+++ litee/src/component/footersec.vue
@@ -1,7 +1,17 @@
 <script setup></script>
 
 <template>
-    
+    <div class="bg-[#FFFFFF] flex w-[1500px] h-[134px] t-[378px] pl-30 pr-30 pt-10 pb-10 gap-15">
+        <div class="font-family-['Work Sans'] font-weight-400 text-[30px] text-[#3E3D53]"><p>Toutes les nouveautés<br>dans votre boîte mail</p></div>
+        <div class="flex pt-10 gap-5">
+            <div class="">
+                <input class="" type="text" placeholder="Email"></input>
+            </div>
+            <div class="">
+                <button class="" type="submit">S'inscrire</button>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped></style>
