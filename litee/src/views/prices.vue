005f8a8959de271fede2882e1734c6237f8254f1
brice825KA <brice825KA@users.noreply.github.com>
2026-09-15 11:05:34 +0100

    main implement prices page

--- litee/src/views/prices.vue
+++ litee/src/views/prices.vue
@@ -2,6 +2,52 @@
 </script>
 
 <template>
+    <div class="space-y-5 items-center justify-center bg-[#F7FAFB00] pl-30 pr-15">
+        <div class="">
+            <p class="font-family-[Work Sans] font-[600] text-[36px] leading-[100%] text-[#000000]">Nos tarifs</p>
+        </div>
+        <div class="">
+            <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000]">Facturation claire et simple. Dites adieu aux<br>
+                frais bancaires que vous ne comprenez pas.
+            </p>
+        </div>
+        <div class="h-[1px] w-[1109px]  bg-[#F2F5F7]"></div>
+    </div>
+    <!--Middle two part-->
+    <div class="flex justify-center items-center gap-10 ">
+        <div class="">
+            <div class="font-family-[Work Sans] font-[100] text-[36px] leading-[235%] text-[#000000]"><p>Particuliers</p></div>
+            <div class="bg-blue-300 w-[514px] h-[813px] t-[541px] l-[195px] shadow-100">
+                <div class="flex justify-center items-center "><img src="../image/man (1)man.png"></div>
+                <p class="font-family-[Work Sans] font-[100] text-[144px] leading-[235%] text-[#000000] text-center">03%</p>
+                <div class="justify-center items-center ">
+                    <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000] text-center">
+                        au rechargement de votre compte<br>
+                        <div class="w-[97px] h-[0px] t-[1029px] l-[395px] border-[3px] bg-[#FA4A4D1A] ml-50 mt-5 justify-center items-center"></div><br>
+                        <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000] text-center">$03 par catrte additionnlle generee</p><br><br>
+                        <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000] text-center">0.5% de frais d transfert</p><br><br>
+                    </p>
+                    <div class="flex justify-center items-center "><button class="w-[172px] h-[55px] t-[1233px] l-[377px] rounded-[27.5px] bg-[#FA4A4D] font-family-[Work Sans] font-[300] text-[18px] leading-[100%] text-[#FFFFFF]">Inscription</button></div>
+                </div>
+            </div>
+        </div>
+        <div class="">
+            <div class="font-family-[Work Sans] font-[100] text-[36px] leading-[235%] text-[#000000]"><p>Entreprises</p></div>
+            <div class="bg-blue-300 w-[514px] h-[813px] t-[541px] l-[195px] shadow-100 ">
+                <div class="flex justify-center items-center "><img src="../image/factoryentreprise.png"></div>
+                <p class="font-family-[Work Sans] font-[100] text-[144px] leading-[235%] text-[#000000] text-center">03%</p>
+                <div class="">
+                    <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000] text-center">
+                        au rechargement de votre compte<br>
+                        <div class="w-[97px] h-[0px] t-[1029px] l-[395px] border-[3px] bg-[#FA4A4D1A] ml-50 mt-5 justify-center items-center"></div><br>
+                        <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000] text-center">$03 par catrte additionnlle generee</p><br><br>
+                        <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000] text-center">$02 / mois par membre d'equipe</p><br><br>
+                    </p>
+                    <div class="flex justify-center items-center "><button class="w-[172px] h-[55px] t-[1233px] l-[377px] rounded-[27.5px] bg-[#FA4A4D] font-family-[Work Sans] font-[300] text-[18px] leading-[100%] text-[#FFFFFF]">Inscription</button></div>
+                </div>
+            </div>
+        </div>
+    </div>
 </template>
 
 <style scoped></style>
