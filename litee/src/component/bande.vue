64fd65bcca95b626483621435f0147b3c6632fb4
brice825KA <brice825KA@users.noreply.github.com>
2026-09-16 12:51:46 +0100

    main fishish animate bande

--- litee/src/component/bande.vue
+++ litee/src/component/bande.vue
@@ -1,34 +1,28 @@
 <script setup lang="ts">
 const items = [
-    '../image/Group 13.png',
-    '../image/Group 16.png',
-    '../image/Group 17.png',
-    '../image/Group 18.png',
-    '../image/Group 19.png',
+    'https://github.com/brice825KA/Litee/blob/main/litee/src/image/Group%2013.png',
+    'https://github.com/brice825KA/Litee/blob/main/litee/src/image/Group%2016.png',
+    'https://github.com/brice825KA/Litee/blob/main/litee/src/image/Group%2017.png',
+    'https://github.com/brice825KA/Litee/blob/main/litee/src/image/Group%2018.png',
+    'https://github.com/brice825KA/Litee/blob/main/litee/src/image/Group%2019.png',
 
-    '../image/Group 13.png',
-    '../image/Group 16.png',
-    '../image/Group 17.png',
-    '../image/Group 18.png',
-    '../image/Group 19.png',
-
-    '../image/Group 13.png',
-    '../image/Group 16.png',
-    '../image/Group 17.png',
-    '../image/Group 18.png',
-    'https://picsum.photos/640/320?random=6'
+    'https://github.com/brice825KA/Litee/blob/main/litee/src/image/Group%2013.png',
+    'https://github.com/brice825KA/Litee/blob/main/litee/src/image/Group%2016.png',
+    'https://github.com/brice825KA/Litee/blob/main/litee/src/image/Group%2017.png',
+    'https://github.com/brice825KA/Litee/blob/main/litee/src/image/Group%2018.png',
 ]
 </script>
 
 <template>
-    <div class="bg-[#F2F5F7">
+    <div class="bg-[#F2F5F7] flex items-center justify-center">
         <UCarousel
         v-slot="{ item }"
         loop
         dots
         auto-scroll
         :items="items"
-        :ui="{ item: 'basis-1/10' }"
+        :ui="{ item: 'basis-1/6' }"
+        class=""
         >
         <img :src="item" width="176" height="90" top="734" left="148" class="" loading="lazy">
         </UCarousel>
