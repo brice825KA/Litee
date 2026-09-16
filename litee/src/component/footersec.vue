8f7e6ace8f4e033e32f68c8eaebab9dad6ebd0e2
brice825KA <brice825KA@users.noreply.github.com>
2026-09-16 16:04:32 +0100

    main correct disposition

--- litee/src/component/footersec.vue
+++ litee/src/component/footersec.vue
@@ -1,14 +1,14 @@
 <script setup></script>
 
 <template>
-    <div class="bg-[#FFFFFF] flex w-[1500px] h-[134px] t-[378px] pl-30 pr-5 pl-70 pt-10 pb-20 mb-5 gap-85">
+    <div class="bg-[#FFFFFF] flex w-[1500px] h-[134px] t-[378px] pl-30 pr-5 pl-70 pt-10 pb-30 mb-5 gap-55">
         <div class="font-family-['Work Sans'] font-weight-400 text-[30px] text-[#3E3D53]"><p>Toutes les nouveautés<br>dans votre boîte mail</p></div>
         <div class="flex pt-10 gap-10">
             <div class="">
-                <input class="pr-30 pt-5 pb-2 border-b-[1px] border-[#D8D8D8]" type="text" placeholder="Email"></input>
+                <input class="pr-50 pt-5 pb-2 border-b-[1px] border-[#D8D8D8]" type="text" placeholder="Email"></input>
             </div>
             <div class="">
-                <button class="cursor-pointer bg-[#FA4A4D] w-[172px] h-[55px] l-[1098px] top-[415px] rounded-[27.5px] font-family-['Work Sans'] font-weight-600 text-[16px] text-[#FFFFFF]" type="submit">S'inscrire</button>
+                <button class="cursor-pointer bg-[#FA4A4D] w-[172px] h-[55px] l-[1098px] top-[415px] rounded-[27.5px] font-family-['Work Sans'] font-[600] text-[16px] text-[#FFFFFF]" type="submit">S'inscrire</button>
             </div>
         </div>
     </div>
