04fa3ef855594cbfd9b2c3092cfb2bf03ea68640
brice825KA <brice825KA@users.noreply.github.com>
2026-09-25 18:01:54 +0100

    Nothing to do

--- litee/src/component/conteneur/prices/prices.vue
+++ litee/src/component/conteneur/prices/prices.vue
@@ -7,46 +7,46 @@ import factory from '../../../assest/image/img_factory_tarifs.webp'
 <template>
     <div class="pricing-header space-y-5 items-center justify-center bg-[#F7FAFB00] pl-70 pr-15">
         <div class="">
-            <p class="font-family-[Work Sans] font-[600] text-[36px] leading-[100%] text-[#000000]">Nos tarifs</p>
+            <p class="font-family-[Work Sans] font-semibold text-[36px] leading-[100%] text-[#000000]">Nos tarifs</p>
         </div>
         <div class="">
-            <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000]">Facturation claire et simple. Dites adieu aux<br>
+            <p class="font-family-[Work Sans] font-light text-[24px] leading-[152%] text-[#000000]">Facturation claire et simple. Dites adieu aux<br>
                 frais bancaires que vous ne comprenez pas.
             </p>
         </div>
-        <div class="h-[1px] w-[1109px]  bg-[#F2F5F7]"></div>
+        <div class="h-px w-277.25  bg-[#F2F5F7]"></div>
     </div>
     <!--Middle two part-->
-    <div class="pricing-cards flex justify-center items-center gap-25 py-5">
+    <div class="pricing-cards flex justify-center items-center gap-80 py-5">
         <div class="pricing-card-block">
-            <div class="font-family-[Work Sans] font-[100] text-[36px] leading-[235%] text-[#000000]"><p>Particuliers</p></div>
-            <div class="w-[514px] h-[813px] t-[541px] l-[195px] shadow-md pb-10">
+            <div class="font-family-[Work Sans] font-thin text-[36px] leading-[235%] text-[#000000]"><p>Particuliers</p></div>
+            <div class="w-128.5 h-203.25 t-[541px] l-[195px] shadow-md pb-10">
                 <div class="flex justify-center items-center "><img :src="man"></div>
-                <p class="font-family-[Work Sans] font-[100] text-[144px] leading-[235%] text-[#000000] text-center">03%</p>
+                <p class="font-family-[Work Sans] font-thin text-[144px] leading-[235%] text-[#000000] text-center">03%</p>
                 <div class="justify-center items-center ">
-                    <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000] text-center">
+                    <p class="font-family-[Work Sans] font-light text-[24px] leading-[152%] text-[#000000] text-center">
                         au rechargement de votre compte<br>
-                        <div class="w-[97px] h-[3px] t-[1029px] l-[395px] bg-[#FA4A4D1A] ml-50 mt-5 justify-center items-center"></div><br>
-                        <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000] text-center">$03 par catrte additionnlle generee</p><br><br>
-                        <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000] text-center">0.5% de frais d transfert</p><br><br>
+                        <div class="w-24.25 h-0.75 t-[1029px] l-[395px] bg-[#FA4A4D1A] ml-50 mt-5 justify-center items-center"></div><br>
+                        <p class="font-family-[Work Sans] font-light text-[24px] leading-[152%] text-[#000000] text-center">$03 par catrte additionnlle generee</p><br><br>
+                        <p class="font-family-[Work Sans] font-light text-[24px] leading-[152%] text-[#000000] text-center">0.5% de frais d transfert</p><br><br>
                     </p>
-                    <div class="flex justify-center items-center "><button class="w-[172px] h-[55px] t-[1233px] l-[377px] rounded-[27.5px] bg-[#FA4A4D] font-family-[Work Sans] font-[300] text-[18px] leading-[100%] text-[#FFFFFF]">Inscription</button></div>
+                    <div class="flex justify-center items-center "><button class="w-43 h-13.75 t-[1233px] l-[377px] rounded-[27.5px] bg-[#FA4A4D] font-family-[Work Sans] font-light text-[18px] leading-[100%] text-[#FFFFFF]">Inscription</button></div>
                 </div>
             </div>
         </div>
         <div class="pricing-card-block">
-            <div class="font-family-[Work Sans] font-[100] text-[36px] leading-[235%] text-[#000000]"><p>Entreprises</p></div>
-            <div class="w-[514px] h-[813px] t-[541px] l-[195px] shadow-md pb-10">
+            <div class="font-family-[Work Sans] font-thin text-[36px] leading-[235%] text-[#000000]"><p>Entreprises</p></div>
+            <div class="w-128.5 h-203.25 t-[541px] l-[195px] shadow-md pb-10">
                 <div class="flex justify-center items-center "><img :src="factory"></div>
-                <p class="font-family-[Work Sans] font-[100] text-[144px] leading-[235%] text-[#000000] text-center">03%</p>
+                <p class="font-family-[Work Sans] font-thin text-[144px] leading-[235%] text-[#000000] text-center">03%</p>
                 <div class="">
-                    <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000] text-center">
+                    <p class="font-family-[Work Sans] font-light text-[24px] leading-[152%] text-[#000000] text-center">
                         au rechargement de votre compte<br>
-                        <div class="w-[97px] h-[3px] t-[1029px] l-[395px]  bg-[#FA4A4D1A] ml-50 mt-5 justify-center items-center"></div><br>
-                        <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000] text-center">$03 par catrte additionnlle generee</p><br><br>
-                        <p class="font-family-[Work Sans] font-[300] text-[24px] leading-[152%] text-[#000000] text-center">$02 / mois par membre d'equipe</p><br><br>
+                        <div class="w-24.25 h-0.75 t-[1029px] l-[395px]  bg-[#FA4A4D1A] ml-50 mt-5 justify-center items-center"></div><br>
+                        <p class="font-family-[Work Sans] font-light text-[24px] leading-[152%] text-[#000000] text-center">$03 par catrte additionnlle generee</p><br><br>
+                        <p class="font-family-[Work Sans] font-light text-[24px] leading-[152%] text-[#000000] text-center">$02 / mois par membre d'equipe</p><br><br>
                     </p>
-                    <div class="flex justify-center items-center "><button class="w-[172px] h-[55px] t-[1233px] l-[377px] rounded-[27.5px] bg-[#FA4A4D] font-family-[Work Sans] font-[300] text-[18px] leading-[100%] text-[#FFFFFF]">Inscription</button></div>
+                    <div class="flex justify-center items-center "><button class="w-43 h-13.75 t-[1233px] l-[377px] rounded-[27.5px] bg-[#FA4A4D] font-family-[Work Sans] font-light text-[18px] leading-[100%] text-[#FFFFFF]">Inscription</button></div>
                 </div>
             </div>
         </div>
